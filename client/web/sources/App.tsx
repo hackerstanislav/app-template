214b60e93884a167603d4dc4b61204054ddefbb9
Stanislav Hacker <hacker.stanislav@gmail.com>
2025-03-27 13:59:07 +0100

    Update

--- client/web/sources/App.tsx
+++ client/web/sources/App.tsx
@@ -3,26 +3,29 @@ import { CookiesProvider } from "./providers/cookies";
 import { StateProvider } from "./providers/state";
 import { RouterProvider } from "./providers/router";
 import { CacheProvider } from "./providers/cache";
+import { ThemeProvider } from "./providers/theme";
 
 import { TELEMETRY_DEBOUNCE } from "./const";
 import Root from "./pages";
 
 function App() {
 	return (
-		<CookiesProvider>
-			<TelemetryProvider
-				Provider={GoogleAnalytics}
-				debounce={TELEMETRY_DEBOUNCE}
-			>
-				<StateProvider>
-					<RouterProvider>
-						<CacheProvider>
-							<Root />
-						</CacheProvider>
-					</RouterProvider>
-				</StateProvider>
-			</TelemetryProvider>
-		</CookiesProvider>
+		<ThemeProvider supportsDarkMode={true}>
+			<CookiesProvider>
+				<TelemetryProvider
+					Provider={GoogleAnalytics}
+					debounce={TELEMETRY_DEBOUNCE}
+				>
+					<StateProvider>
+						<RouterProvider>
+							<CacheProvider>
+								<Root />
+							</CacheProvider>
+						</RouterProvider>
+					</StateProvider>
+				</TelemetryProvider>
+			</CookiesProvider>
+		</ThemeProvider>
 	);
 }
 
--- client/web/sources/pages/index.tsx
+++ client/web/sources/pages/index.tsx
@@ -1,18 +1,16 @@
 import { CssBaseline } from "@mui/material";
 import GlobalStyles from "@mui/material/GlobalStyles";
 
-import { ThemeProvider } from "../providers/theme";
-
 import { GLOBAL_STYLES } from "./index.styles";
 import Default from "./Default";
 
 function Root() {
 	return (
-		<ThemeProvider supportsDarkMode={true}>
+		<>
 			<CssBaseline />
 			<GlobalStyles styles={GLOBAL_STYLES} />
 			<Default />
-		</ThemeProvider>
+		</>
 	);
 }
 
