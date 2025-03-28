2cfd48090648292adc9f3405e929f3895bea6046
Stanislav Hacker <hacker.stanislav@gmail.com>
2025-03-28 20:31:48 +0100

    Description of template

--- server/main/implementation/db.ts
+++ server/main/implementation/db.ts
@@ -3,10 +3,26 @@ import { Express } from "express";
 
 import { ServerState } from "../common/state";
 
+/**
+ * Database store type, used to specify the database store and real implementation.
+ */
+export type DatabaseStore = unknown;
+
+/**
+ * Create a database store.
+ *
+ * This function is a placeholder for the real implementation of the database store. For now it just logs an error
+ * about the missing implementation. There need to be a real implementation of the database store in the final
+ * application that can be used to interact with the database.
+ *
+ * @param app - The express app.
+ * @param state - The server state.
+ * @returns A function that creates a database store.
+ */
 export function createDatabase(
 	app: Express,
 	state: ServerState,
-): (db: Db) => unknown {
+): (db: Db) => DatabaseStore {
 	return () => {
 		state.logger.logger?.error({
 			message: "Database store must be implemented in real application.",
--- server/main/implementation/routes.ts
+++ server/main/implementation/routes.ts
@@ -3,6 +3,17 @@ import { Express, Router } from "express";
 import { RouterHandlers } from "../common/routes";
 import { ServerState } from "../common/state";
 
+/**
+ * Create routes for the application.
+ *
+ * This function is a placeholder for the real implementation of the routes. For now it just logs an error about the
+ * missing implementation. There need to be a real implementation of the routes in the final application that can be
+ * used to define the routes for the application. By default, login, logout, register and invite routes are defined
+ * in  this template application.
+ *
+ * @param app - The express app.
+ * @param state - The server state.
+ */
 export function createRoutes(
 	app: Express,
 	state: ServerState,
